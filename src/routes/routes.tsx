0dca302cb1893c9aaa836a8ddbec19335343273d
Usman Khan <mdusmanhanifkhan@gmail.com>
2026-01-04 19:39:12 +0500

    set Live url

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,35 +1,5 @@
 import { routePaths } from '../constants/routePaths'
 
-// export const SidebarRoutes = [
-//   { name: "Dashboard", link: routePaths.DASHBOARD },
-//   { name: "Patient", link: routePaths.PATIENTS , permission: "canManagePatients"},
-//   { name: "Departments", link: routePaths.DEPARTMENTS },
-//   { name: "Procedure", link: routePaths.PROCEDURE },
-//   { name: "Doctors", link: routePaths.DOCTORS },
-//   { name: "Doctor Fee", link: routePaths.DOCTOR_FEE },
-//   { name: "Welfare Management", link: routePaths.WELFARE_MANAGEMENT },
-//   { name: "Billing & Receipts", link: routePaths.BILLING },
-//   // { name: "Appointments", link: routePaths.APPOINTMENTS },
-//   // { name: "Vitals Check", link: routePaths.VITALS },
-//   // { name: "Consultation", link: routePaths.CONSULTATION },
-//   // { name: "Pharmacy", link: routePaths.PHARMACY },
-//   // { name: "Laboratory", link: routePaths.LABORATORY },
-//   // { name: "Inventory / Store", link: routePaths.INVENTORY },
-//   // {
-//   //   name: "Reports",
-//   //   link: routePaths.REPORTS,
-//   //   children: [
-//   //     { name: "Daily Visits", link: routePaths.REPORTS_DAILY_VISITS },
-//   //     { name: "Billing Summary", link: routePaths.REPORTS_BILLING },
-//   //     { name: "Doctor-wise", link: routePaths.REPORTS_DOCTOR_WISE },
-//   //     { name: "Department-wise", link: routePaths.REPORTS_DEPARTMENT_WISE },
-//   //     { name: "Pharmacy Usage", link: routePaths.REPORTS_PHARMACY },
-//   //     { name: "Lab Summary", link: routePaths.REPORTS_LAB },
-//   //   ],
-//   // },
-//   { name: "User Management", link: routePaths.USERS_MANAGEMENT },
-//   { name: "Settings", link: routePaths.SETTINGS },
-// ];
 export const SidebarRoutes = [
   {
     name: 'Dashboard',
@@ -62,7 +32,7 @@ export const SidebarRoutes = [
   {
     name: 'Patient History',
     link: routePaths.PATIENT_HISTORY,
-    // permission: 'canManagePatientsHistory',
+    permission: 'canManagePatientsHistory',
     icon: (
       <svg
         className="w-[15px] h-[15px]"
