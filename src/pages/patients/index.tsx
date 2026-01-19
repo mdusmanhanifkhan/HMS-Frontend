f221268f4fe78d008a30052193a335a986894e60
Usman Khan <mdusmanhanifkhan@gmail.com>
2026-01-20 01:21:56 +0500

    change time in patient index.tsx file

--- src/pages/patients/index.tsx
+++ src/pages/patients/index.tsx
@@ -292,11 +292,18 @@ const PatientRow = ({
     <td className="px-6 py-2">{patient.age}</td>
     <td className="px-6 py-2">{patient.cnicNumber || '-'}</td>
     <td className="px-6 py-2">{patient.phoneNumber || '-'}</td>
-    <td className="px-6 py-2">
-      {patient?.createdAt
-        ? new Date(patient?.createdAt).toLocaleDateString()
-        : '-'}
-    </td>
+   <td className="px-6 py-2">
+  {patient?.createdAt
+    ? new Date(patient.createdAt).toLocaleString('en-GB', {
+        day: '2-digit',
+        month: '2-digit',
+        year: 'numeric',
+        hour: '2-digit',
+        minute: '2-digit',
+      })
+    : '-'}
+</td>
+
 
     <td className="px-6 py-2 flex items-center gap-2">
       {role === 'superadmin' && (
