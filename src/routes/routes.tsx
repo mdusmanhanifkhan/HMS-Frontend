e0285826017d22e7d21760ebb734ca0b3a39532c
Usman Khan <mdusmanhanifkhan@gmail.com>
2026-02-02 16:57:56 +0500

    Update routes.tsx

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -214,7 +214,7 @@ export const SidebarRoutes = [
   {
     name: 'Patient Report',
     link: routePaths.PATIENT_REPORT,
-    permission: 'canViewReports',
+   // permission: 'canViewReports',
     icon: (
       <svg
         className="w-[18px] h-[18px]"
